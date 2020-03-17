225fb7a5a79fd11427ca56fdcf7f6201462e42ac
mickael2054 <mickaelgerber@gmail.com>
2020-03-17 21:12:33 +0100

    Add lib files

---
+++ IReadiTunes/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Mickael Gerber <mickaelgerberdev@gmail.com>
+
+MIT License
+    https://github.com/mickael2054/IReadiTunes.git
+"""
+
+name          = "IReadiTunes"
+__author__    = "Mickael Gerber <mickaelgerberdev@gmail.com>"
+__version__   =  "1.0.0"
+__license__   = "MIT"
---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+ 
+import IReadiTunes
+
+setup(
+    name='IReadiTunes',
+    version=IReadiTunes.__version__,
+    packages=find_packages(),
+    author="Mickael Gerber",
+    author_email="mickaelgerberdev@gmail.com",
+	description="Tool to get any information about iTunes tracks and playlists quickly and easily",
+	long_description_content_type = "text/markdown",
+	long_description=open('README.md').read(),
+    url='https://github.com/mickael2054/IReadiTunes.git',
+	install_requires=[
+        'xml.etree.ElementTree',
+		'urllib.parse',
+		  ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+    ],
+    license="MIT", 
+)
